cd992f90629bb7e38ece9d69920efba5486debed
goat <16760685+goaaats@users.noreply.github.com>
2021-03-21 15:37:34 +0100

    version 0.1.1

--- Goaaats.DeathTracker/Properties/AssemblyInfo.cs
+++ Goaaats.DeathTracker/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModTemplate")]
+[assembly: AssemblyTitle("Goaaats.DeathTracker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModTemplate")]
+[assembly: AssemblyProduct("Goaaats.DeathTracker")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
